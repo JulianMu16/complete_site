dc1bb9ee5bb9a5a1a4d68402dd3f52137cec241f
Julian Mueller <113564385+JulianMu16@users.noreply.github.com>
2023-11-21 22:18:14 -0500

    Add files via upload

---
+++ js/audio_swap.js
@@ -0,0 +1,4 @@
+function audio_swap() {
+    console.log("clicked!")
+    document.getElementById("mainAudio").src = "audio/John Lopker _ Popular USA Majority - Kill Jill _ Unbiblical Dress.mp3"
+}   
---
+++ js/image_swap.js
@@ -0,0 +1,4 @@
+function image_swap() {
+    console.log("clicked!")
+    document.getElementById("mainImg").src = "images/VenezuelanFlag.png"
+}
